f3ba131a7fdfe60807593e23b9cf98a2823b49e1
Gamertube583yt <49451466+Gamertube583yt@users.noreply.github.com>
2021-10-12 10:37:11 +0500

    FileUpdated

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import discord
 class Main:
 	def __init__(self):
 		print('[LOG] Module EasyBotGame initalized successfully and successfully updated!')
-		print('TEST краш!!!')
+		print('[LOG] File main.py successfully updated!')
 	def Random(self, min, max):
 		self.min = min
 		self.max = max
